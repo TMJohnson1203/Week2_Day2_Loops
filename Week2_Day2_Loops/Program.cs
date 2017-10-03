670a90657c9031424189ce4067525bb29df24dcc
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-03 11:56:47 -0400

    initiial commit

--- Week2_Day2_Loops/Program.cs
+++ Week2_Day2_Loops/Program.cs
@@ -10,6 +10,15 @@ namespace Week2_Day2_Loops
     {
         static void Main(string[] args)
         {
+
+            for(int i = 0; i <= 10; i += 2) ;
+            // Console.Writeine(i);
+
+            for (int counter = 50; counter <= 100; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+                
         }
     }
 }
