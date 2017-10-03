70ba575a4588e8433d46e40b84b46e6c2a4c3721
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-03 13:30:27 -0400

    initial commit

--- Week2_Day2_Loops/Program.cs
+++ Week2_Day2_Loops/Program.cs
@@ -11,13 +11,33 @@ namespace Week2_Day2_Loops
         static void Main(string[] args)
         {
 
-            for(int i = 0; i <= 10; i += 2) ;
-            // Console.Writeine(i);
+            //for(int i = 0; i <= 10; i += 2) ;
+            //// Console.Writeine(i);
 
-            for (int counter = 50; counter <= 100; counter++)
+            //for (int counter = 50; counter <= 100; counter++)
+            //{
+            //    //    Console.WriteLine(counter);
+            //}
+
+            //string greetings = "My name is Teresa.";
+            //string[] words = greetings.Split();
+
+            //for(int i =0; i < greetings.Length;i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //}
+
+            for(int i = 1; i <=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int counter = 50; counter <=100; counter++)
             {
                 Console.WriteLine(counter);
             }
+
+
                 
         }
     }
